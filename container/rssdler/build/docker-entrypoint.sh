e6ac690d81094c0a146c76622047046d950f09a4
Sebastian Hutter <huttersebastian@gmail.com>
2015-07-26 09:44:58 +0200

    renamed ansible to build

--- container/rssdler/build/docker-entrypoint.sh
+++ container/rssdler/build/docker-entrypoint.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 set -e
 
+echo running entrypoint script
+echo who am im
+id
+
+echo parameters
+echo $@
+
 # if the first paramter is rssdler start the
 # rssdler script
 if [ "$1" = 'rssdler' ]; then
+    # shift the parametes by one
+    shift
+
+    echo received rssdler as parameters
+    echo remaining parameters
+
+    echo $@
 
     # if the configuration file does not exist copy it
     if [ ! -f /var/lib/rssdler/rssdler.conf ]; then
@@ -11,7 +25,7 @@ if [ "$1" = 'rssdler' ]; then
     fi
 
     # first paraemter is always rssdler = /usr/bin/rssdler xxx
-    exec "/usr/bin/$@"
+    exec "/usr/bin/rssdler" "$@"
 fi
 
 # if the first paramter is not rssdler start
