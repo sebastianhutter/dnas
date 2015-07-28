ef8e667c7b47406c1eb07a54f9f49c4bba4373db
Sebastian Hutter <huttersebastian@gmail.com>
2015-07-28 16:48:23 +0200

    added basic couchpotato container

---
+++ container/couchpotato/build/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+cmd=$1
+shift
+param="$@"
+
+# if the first paramter is plex start the
+# plex media service
+if [ "$cmd" = 'couchpotato' ]; then
+  # the plexmedia server can be started by simply running the start script
+  # in the plexmedia server lib directory
+  cd
+  exec python /opt/couchpotato/CouchPotato.py $param
+fi
+
+# if the first paramter is not plex start
+# whatever parameters where given
+if [ -z "$param" ]; then
+    exec "$cmd"
+else
+    exec "$cmd" $param
+fi
