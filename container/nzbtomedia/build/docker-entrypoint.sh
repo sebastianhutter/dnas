c1025e96f25e2bb703115110f936f8a57eaccbac
Sebastian Hutter <huttersebastian@gmail.com>
2015-07-28 20:14:13 +0200

    added nzbtomedia container skeleton

---
+++ container/nzbtomedia/build/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+cmd=$1
+shift
+param="$@"
+
+# if the first paramter is plex start the
+# plex media service
+if [ "$cmd" = 'nzbtomedia' ]; then
+  # the nzbtomedia container only serves up the necessary
+  # scripts for sabnzbd to execute after a successfull or
+  # unsuccessfull download
+  /bin/true
+fi
+
+# if the first paramter is not plex start
+# whatever parameters where given
+if [ -z "$param" ]; then
+    exec "$cmd"
+else
+    exec "$cmd" $param
+fi
