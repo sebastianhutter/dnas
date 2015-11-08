3bdc33c37a76d9b51f9d7b8d35133afac43b4cb2
Sebastian Hutter <huttersebastian@gmail.com>
2015-11-08 16:42:22 +0100

    basic openvpn as docker container

---
+++ container/openvpnas/build/docker-entrypoint.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -e
+
+usage()
+{
+cat << EOF
+usage: $0 options
+
+This is the entry point for the docker container.
+It takes multiple argumens=ts
+
+OPTIONS:
+   -h      Show this message
+   -c      the command to run (by default its set to run the dockerized service)
+
+EOF
+}
+
+CMD="openvpnas"
+
+while getopts "hc:" OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         c)
+             CMD=$OPTARG
+             ;;
+         ?)
+             usage
+             exit
+             ;;
+     esac
+done
+shift $(expr $OPTIND - 1 )
+param="$@"
+
+
+if [ "$CMD" = 'openvpnas' ]; then
+
+  # remove sock files (the ...../etc folder is usually mounted as persistent volume)
+  for sf in $sockfile ; do
+      if [ -e "$sf" ]; then
+        rm -f "$sf" &>/dev/null
+      fi
+  done
+
+  # run the openvpn service
+  # for openvpn to work properly we need iptables
+  # add --cap-add=NET_ADMIN --cap-add=NET_RAW --device=/dev/net/tun to your docker command line to make it work
+  exec sudo -u root /usr/local/openvpn_as/scripts/openvpnas --logfile=- --pidfile=/var/run/openvpnas.pid --nodaemon
+fi
+
+# if the first paramter is not plex start
+# whatever parameters where given
+if [ -z "$param" ]; then
+    exec "$CMD"
+else
+    exec "$cmd" $param
+fi
