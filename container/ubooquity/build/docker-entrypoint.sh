a200b14cf8710f83d91e92b4934b63f6d9cb7dc2
Sebastian Hutter <mail@sebastian-hutter.ch>
2015-11-30 00:18:38 +0100

    commit issue

--- container/ubooquity/build/docker-entrypoint.sh
+++ container/ubooquity/build/docker-entrypoint.sh
@@ -27,11 +27,7 @@ USERNAME=""
 PASSWORD=""
 URL=""
 
-<<<<<<< HEAD
 while getopts "hc:nmqu:p:f:d:i:o:" OPTION
-=======
-while getopts "hc:nu:p:f:" OPTION
->>>>>>> origin/master
 do
      case $OPTION in
          h)
