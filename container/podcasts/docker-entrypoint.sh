489d87d54ff9c0b95faf107cce5f8e4a19531194
Sebastian Hutter <shutter@nucf.barfoot.local>
2015-12-27 22:56:04 +0100

    added annex mode in entrypoint script

--- container/podcasts/docker-entrypoint.sh
+++ container/podcasts/docker-entrypoint.sh
@@ -3,12 +3,24 @@
 podcasts_dir="/volumes/podcasts"
 config_dir="/volumes/config"
 
-# variables set by 
+# variables set by docker
+annex_mode="$ANNEXMODE"
 template="$FEEDTEMPLATE"
 netrc_url="$AUTHURL"
 podcasts_url="$PODCASTSURL"
 
+
 echo started podcast container | ts '%F %T'
+
+echo check annex mode
+if [ -z "$annex_mode" ]; then
+  echo annex mode is indirect | ts '%F %T'
+  annex_mode="indirect"
+else
+  echo annex mode is direct | ts '%F %T'
+  wget -q $netrc_url -O "$config_dir/netrc"
+  annex_mode="direct"
+fi
 
 echo downloading authentication file | ts '%F %T'
 if [ -z "$netrc_url" ]; then
@@ -32,6 +44,7 @@ echo initialising repository in $podcasts_dir | ts '%F %T'
 cd "$podcasts_dir"
 git init --quiet 
 git annex init --quiet
+git annex $annex_mode --quiet
 echo re-initialized repository | ts '%F %T'
 
 echo importing podcasts from $config_dir/podcasts - with auth from $config_dir/netrc | ts '%F %T'
