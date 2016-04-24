05eeb29433cce82e6fa49539d07efd113722b0f4
Sebastian Hutter <mail@sebastian-hutter.ch>
2016-04-24 21:49:12 +0200

    force mv command

--- container/nginx/build/docker-entrypoint.sh
+++ container/nginx/build/docker-entrypoint.sh
@@ -38,7 +38,7 @@ fi
 
 # now parse the nginx configuration file with j2
 echo "Parse the nginx configuration file with j2"
-mv "$CONFIG_LOCAL" "$CONFIG_LOCAL.orig"
+mv -f "$CONFIG_LOCAL" "$CONFIG_LOCAL.orig"
 j2 "$CONFIG_LOCAL.orig" > "$CONFIG_LOCAL"
 [ $? -ne 0 ] && exit 1
 
